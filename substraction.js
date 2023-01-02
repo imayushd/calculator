5dab9ea5cb4d24dea822e72782dfe6b6ee48f553
imayushd <ayush1496dixit@gmail.com>
2023-01-02 11:00:54 +0530

    substraction is added

--- substraction.js
+++ substraction.js
@@ -1,1 +1,8 @@
-This is substration file
+function subtract() {
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result
+result = a-b
+document.getElementById("result").value=result
+}
+
