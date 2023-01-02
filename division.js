ca20706c451974ca5861e9b9fe75698e241610b2
BinduPuneeth <bindurameshgowda1997@gmail.com>
2023-01-02 11:07:07 +0530

    division.js added

--- division.js
+++ division.js
@@ -1,1 +1,8 @@
-this is divisio
+function division()
+{
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result
+result  = a/b
+document.getElementById("result").value = result
+}
