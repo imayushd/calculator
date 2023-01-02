fa03035754ffb5f944710b8ed8985a9cfbf9c501
AlthafHussen <althafnh786@gmail.com>
2023-01-02 11:01:35 +0530

    addition code is added

--- addition.js
+++ addition.js
@@ -1,1 +1,6 @@
-this is js file
+function add() {
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result = parseInt (a) + parseInt (b)
+document.getElementById("result").value=result
+}
